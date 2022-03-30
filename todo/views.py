58a8424d4dd5cc6fb9617433b340ab492fdf408a
Ricardo Costa <ra.silva.dcosta@gmail.com>
2022-03-30 19:42:37 +0200

    created the model Todo

--- todo/views.py
+++ todo/views.py
@@ -13,7 +13,7 @@ def home(request):
 
 def signupuser(request):
     if request.method == 'GET':
-        return render(request, 'todo/signupuser.html',{'form':UserCreationForm()})
+        return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
