a2b1c2d70f41ad06c8dd697e5de2ee96ea4b27ec
Ricardo Costa <ra.silva.dcosta@gmail.com>
2022-03-30 19:48:38 +0200

    Migration on tdo model, adding blank=true

--- todo/models.py
+++ todo/models.py
@@ -5,6 +5,6 @@ class Todo(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField(blank=True)
     important = models.BooleanField(default=False)
-    datecompleted = models.DateTimeField(null=True)
+    datecompleted = models.DateTimeField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
